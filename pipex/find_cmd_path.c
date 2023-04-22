a6bbc4036baaedee55039bec5bb1c3582edf179b
Warre Devriese <wdevries@a1-r2-p3.s19.be>
2023-04-22 17:43:14 +0200

    Update

--- pipex/find_cmd_path.c
+++ pipex/find_cmd_path.c
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 12:45:54 by wdevries          #+#    #+#             */
-/*   Updated: 2023/04/22 16:52:02 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/04/22 17:42:54 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	ft_free_arr(char **dirs)
 
 	i = 0;
 	while (dirs[i])
-		free(dirs[i++]);
+		free(dirs[--i]);
 	free(dirs);
 }
 
@@ -69,6 +69,7 @@ static char	*search_cmd_in_dirs(char *path_env, char *cmd)
 	char	**dirs;
 
 	full_path = NULL;
+	//buffer might me reduntant
 	buffer_split = ft_strdup(path_env);
 	dirs = ft_split(buffer_split, ':');
 	free(buffer_split);
@@ -80,6 +81,7 @@ static char	*search_cmd_in_dirs(char *path_env, char *cmd)
 		free(full_path);
 		full_path = NULL;
 	}
+	//error occrus because I iterate through dirs before calling ft_fgree_dirs
 	ft_free_arr(dirs);
 	if (!full_path)
 	{
