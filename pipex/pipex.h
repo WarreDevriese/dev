91bbbf52be96cf85c21088c6cff63670d1d952f8
Warre Devriese <wdevries@a1-r2-p3.s19.be>
2023-04-22 16:57:51 +0200

    Update

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 13:49:21 by wdevries          #+#    #+#             */
-/*   Updated: 2023/04/22 16:47:40 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/04/22 16:57:46 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ void	ft_close(int fd);
 char	*find_cmd_path(char **argv, char **envp);
 void	child2(int pipefd[], pid_t pid2, char **argv, char **envp);
 void	child1(int pipefd[], pid_t pid1, char **argv, char **envp);
+int		main(int argc, char **argv, char **envp);
 
 #endif
