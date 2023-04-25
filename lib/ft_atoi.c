a459872f1d59f781fc05249adf903d4716406440
Warre Devriese <wdevries@a1-r2-p3.s19.be>
2023-04-25 16:48:21 +0200

    Update

--- lib/ft_atoi.c
+++ lib/ft_atoi.c
@@ -6,31 +6,29 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 16:56:34 by wdevries          #+#    #+#             */
-/*   Updated: 2023/04/03 17:17:16 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/04/25 16:48:15 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
 	int	sign;
 	int	result;
 
-	i = 0;
 	sign = 1;
 	result = 0;
-	while (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r'))
-		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
+	while (*nptr == ' ' || (*nptr >= '\t' && *nptr <= '\r'))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
 	{
-		if (nptr[i] == '-')
+		if (*nptr == '-')
 			sign = -1;
-		i++;
+		nptr++;
 	}
-	while (nptr[i] >= '0' && nptr[i] <= '9')
+	while (*nptr >= '0' && *nptr <= '9')
 	{
-		result = result * 10 + nptr[i] - '0';
-		i++;
+		result = result * 10 + *nptr - '0';
+		nptr++;
 	}
 	return (sign * result);
 }
