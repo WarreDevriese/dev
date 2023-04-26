a05a7fd1809554e558bcb35b2ccaad52cf855148
Warre Devriese <wdevries@a1-r2-p3.s19.be>
2023-04-26 10:25:40 +0200

    Update

--- lib/ft_split.c
+++ lib/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 15:36:02 by wdevries          #+#    #+#             */
-/*   Updated: 2023/04/07 13:23:09 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/04/26 10:25:30 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,29 @@
 static size_t	ft_word_count(char const *s, char c)
 {
 	size_t	count;
+	int		in_word;
 
 	count = 0;
+	in_word = 0;
 	while (*s)
 	{
-		while (*s == c)
-			s++;
-		if (*s)
+		if (*s != c && !in_word)
 		{
 			count++;
-			while (*s && *s != c)
-				s++;
+			in_word = 1;
 		}
+		else if (*s == c)
+		{
+			in_word = 0;
+		}
+		s++;
 	}
 	return (count);
 }
 
 static void	ft_free_split(char **split, size_t i)
 {
-	while (i > 0)
+	while (i)
 		free(split[--i]);
 	free(split);
 }
@@ -56,11 +60,11 @@ static char	**ft_fill_split(char **split, char const *s,
 		if (!split[i])
 		{
 			ft_free_split(split, i);
-			return (0);
+			return (NULL);
 		}
 		i++;
 	}
-	split[i] = (void *)0;
+	split[i] = NULL;
 	return (split);
 }
 
@@ -72,7 +76,7 @@ char	**ft_split(char const *s, char c)
 	word_count = ft_word_count(s, c);
 	split = (char **)malloc((word_count + 1) * sizeof(char *));
 	if (!split)
-		return (0);
+		return (NULL);
 	ft_fill_split(split, s, c, word_count);
 	return (split);
 }
