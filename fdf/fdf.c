b185fbf42845b7f8d08a12be7e839b35662cfc70
Warre Devriese <wdevries@a1-r2-p3.s19.be>
2023-04-28 19:47:36 +0200

    Update

--- fdf/fdf.c
+++ fdf/fdf.c
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 15:14:04 by wdevries          #+#    #+#             */
-/*   Updated: 2023/04/28 18:46:53 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/04/28 19:47:22 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	get_dimensions(char	*file, t_dimensions *dimensions)
 	while (1)
 	{
 		line = get_next_line(fd);
+		//add error handle. errno?
 		if (!line)
 			break ;
 		temp_width = ft_word_count(line, ' ');
@@ -35,17 +36,56 @@ void	get_dimensions(char	*file, t_dimensions *dimensions)
 	ft_close(fd);
 }
 
+int		**allocate_2d_array(size_t width, size_t height)
+{
+	int		**data;
+	size_t	i;
+
+	data = (int **)malloc(height * sizeof(int *));
+	if (!data)
+	{
+		perror("Error malloc 2D array");
+		exit(1);
+	}
+	i = 0;
+	while (i < height)
+	{
+		data[i] = (int *)malloc(width * sizeof(int));
+		if (!data[i])
+		{
+			while (i)
+				free(data[i--]);
+			free(data);
+			perror("Error malloc 2D array");
+			exit(1);
+		}
+	}
+	return (data);
+}
+
+int		**parse_map(file, dimensions)
+{
+	int		**data;
+	int		fd;
+	size_t	i;
+
+	data = allocate_2d_array(dimensions.widthm dimensions.height);
+	ft_open_rdonly(file, &fd);
+	i = 0;
+	while (i < dimensions.height)
+	{
+
+	}
+	ft_close(fd);
+	return (data);
+}
+
 int		main(int argc, char **argv)
 {
 	t_dimensions	dimensions;
 
-	if (argc != 2)
-	{
-		perror("Usage: ./fdf <filename>");
-		exit(1);
-	}
+	//add error handling for input
+	(void)argc;
 	get_dimensions(argv[1], &dimensions);
-	printf("Width: %i\n", (int)dimensions.width);
-	printf("Height: %i\n", (int)dimensions.height);
 	return (0);
 }
--- lib/ft_word_count.c
+++ lib/ft_word_count.c
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 17:17:04 by wdevries          #+#    #+#             */
-/*   Updated: 2023/04/28 17:41:14 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/04/28 19:06:04 by wdevries         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 size_t	ft_word_count(char const *s, char c)
 {
 	size_t	count;
+	char const	*start;
 
 	count = 0;
+	start = s;
 	while (*s)
 	{
-		if (*s != c && (s == 0 || *(s - 1) == c))
+		if (*s != c && (s == start || *(s - 1) == c))
 			count++;
 		s++;
 	}
