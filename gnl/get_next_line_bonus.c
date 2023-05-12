03ebdc54b175ebfac5c4b835817daefd87a05a04
Warre Devriese <wdevries@a1-r2-p3.s19.be>
2023-05-12 16:02:48 +0200

    Update

---
+++ gnl/get_next_line_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 15:24:40 by wdevries          #+#    #+#             */
+/*   Updated: 2023/05/12 15:56:04 by wdevries         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*free_line(char **line)
+{
+	if (*line)
+	{
+		free(*line);
+		*line = NULL;
+	}
+	return (NULL);
+}
+
+static int	ft_read(int fd, char **line_parse)
+{
+	char	*buffer;
+	char	*new_line_parse;
+	ssize_t	bytes_read;
+
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (-1);
+	bytes_read = 1;
+	while (!ft_strchr(*line_parse, '\n') && bytes_read)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buffer);
+			return (-1);
+		}
+		buffer[bytes_read] = '\0';
+		new_line_parse = ft_strjoin(*line_parse, buffer);
+		free(*line_parse);
+		*line_parse = new_line_parse;
+	}
+	free(buffer);
+	return (0);
+}
+
+static char	*ft_extract(char **line_parse)
+{
+	char	*nl_position;
+	char	*line_return;
+	char	*new_line_parse;
+
+	nl_position = ft_strchr(*line_parse, '\n');
+	if (nl_position)
+	{
+		new_line_parse = ft_strdup(nl_position + 1);
+		*(nl_position + 1) = '\0';
+		line_return = ft_strdup(*line_parse);
+		free_line(line_parse);
+		*line_parse = new_line_parse;
+	}
+	else
+	{
+		line_return = *line_parse;
+		*line_parse = NULL;
+	}
+	return (line_return);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*line_parse;
+	char		*line_return;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	if (ft_read(fd, &line_parse) == -1)
+		return (free_line(&line_parse));
+	if (!line_parse)
+		return (NULL);
+	if (line_parse && *line_parse == 0)
+		return (free_line(&line_parse));
+	line_return = ft_extract(&line_parse);
+	return (line_return);
+}
---
+++ gnl/get_next_line_utils_bonus.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 15:25:06 by wdevries          #+#    #+#             */
+/*   Updated: 2023/05/12 15:56:03 by wdevries         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == (char)c)
+		return ((char *)s);
+	return (0);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		len;
+	char	*p;
+	char	*p_start;
+
+	len = ft_strlen(s);
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	p_start = p;
+	while (s && *s)
+		*p++ = *s++;
+	*p = '\0';
+	return (p_start);
+}
+
+char	*ft_strjoin(char const *line_parse, char const *buffer)
+{
+	size_t	len;
+	char	*new_s;
+	char	*new_s_start;
+
+	if (!line_parse && !buffer)
+		return (NULL);
+	len = ft_strlen(line_parse) + ft_strlen(buffer);
+	new_s = (char *)malloc((len + 1) * sizeof(char));
+	if (!new_s)
+		return (NULL);
+	new_s_start = new_s;
+	while (line_parse && *line_parse)
+		*new_s++ = *line_parse++;
+	while (buffer && *buffer)
+		*new_s++ = *buffer++;
+	*new_s = '\0';
+	return (new_s_start);
+}
