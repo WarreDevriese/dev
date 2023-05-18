1daa8d7968d4567cf408a8708e52a3d9ffde47cb
WarreDevriese <devriese.warre@gmail.com>
2023-05-18 22:03:09 +0200

    Update

--- psw/push_swap.h
+++ psw/push_swap.h
@@ -6,56 +6,72 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 15:02:15 by wdevries          #+#    #+#             */
-/*   Updated: 2023/05/18 17:14:38 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/05/18 22:03:05 by warredevriese    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+//STACK ID'S
 # define A = 0
 # define B = 1
 
-typedef enum	e_operations
-{
-	SA,
-	SB,
-	SS,
-	PA,
-	PB,
-	RA,
-	RB,
-	RR,
-	RRA,
-	RRB,
-	RRR
-}				t_operations;
+//OPERATION ID'S
+//nullifying operations cancel out (double swaps do not happen)
+# define SS = 0 
+# define SA = 1 
+# define SB = 2
+
+# define RA = -3
+# define RRA = 3
+
+# define RB = -4
+# define RRB = 4
+
+# define RR = -5
+# define RRR = 5
+
+# define PA = -6
+# define PB = 6
 
 typedef enum	e_cases
 {
-	CASE1,
-	CASE2,
-	CASE3,
-	CASE4
+	CASE1,		//RR
+	CASE2, 		//RRR
+	CASE3, 		//RA + RRB
+	CASE4  		//RRA + RB
 }				t_cases;
 
-//index: normalized value of input;
-//pos: desired position in opposite stack;
-//cost: min amount of operations to achieve pos;
-//CASE: set of operations to achieve pos;
-typedef struct	s_element
+typedef struct	s_params
 {
-	unsigned short	index;
-	unsigned short	pos;
-	unsigned short	cost;
-	unsigned short 	CASE;
-}				t_element;
+	short	cost;
+	short 	CASE;
+	short	from_pos;
+	short	to_pos;
+}				t_params;
 
-//size: current amount of elements in stack;
 typedef struct	s_stack
 {
-	t_element		*array;
-	unsigned short	size;
+	short	*array;
+	short	size;
 }				t_stack;
 
 #endif
+
+
+/* typedef enum	e_actions */
+/* { */
+/* 	SA, */
+/* 	SB, */
+/* 	SS, */
+/* 	PA, */
+/* 	PB, */
+/* 	RA, */
+/* 	RB, */
+/* 	RR, */
+/* 	RRA, */
+/* 	RRB, */
+/* 	RRR */
+/* }				t_actions; */
+
