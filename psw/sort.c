42feb5ec2c57a1bc3b9698b9b3c7d35819297ed4
WarreDevriese <devriese.warre@gmail.com>
2023-05-21 11:46:59 +0200

    Update

--- psw/sort.c
+++ psw/sort.c
@@ -6,7 +6,7 @@
 /*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:57:40 by wdevries          #+#    #+#             */
-/*   Updated: 2023/05/20 15:35:48 by wdevries         ###   ########.fr       */
+/*   Updated: 2023/05/21 11:46:41 by warredevriese    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,20 @@ bool	ft_is_sorted(t_stacks *stacks)
 /* void	ft_sort(t_stack *stacks) */
 /* { */
 /* } */
+
+static signed char	*ft_construct_operation_string(short cmd1_num, signed char cmd1,
+  							short cmd2_num, signed char cmd2) 
+{
+	static signed char		o_string[20000];
+	static short			pos; 
+	short					i;
+
+	pos = 0;
+	while (cmd1_num-- && pos < 20000)
+		o_string[pos++] = cmd1;
+	while (cmd2_num-- && pos < 20000)
+		o_string[pos++] = cmd2;
+	if (pos == 19999)
+		write(1, "Dude what are you doing", 24);
+	return (o_string);
+}	
