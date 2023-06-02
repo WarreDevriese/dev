61970b7d0210ab1691b4242858bbf8befa4a1435
Warre Devriese <wdevries@a1-r2-p3.s19.be>
2023-06-02 12:43:06 +0200

    Update

---
+++ philo/philo.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 09:36:06 by wdevries          #+#    #+#             */
+/*   Updated: 2023/06/02 12:42:39 by wdevries         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+typedef struct	s_params
+{
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
+	pthread_t		*philosophers;
+	pthread_mutex_t	*forks;
+}				t_params;
+
+int		ft_atoi(const char *nptr);
+
+#endif
---
+++ philo/utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wdevries <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/02 10:24:26 by wdevries          #+#    #+#             */
+/*   Updated: 2023/06/02 10:25:41 by wdevries         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	result;
+
+	if (!nptr)
+		return (0);
+	sign = 1;
+	result = 0;
+	while (*nptr == ' ' || (*nptr >= '\t' && *nptr <= '\r'))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr >= '0' && *nptr <= '9')
+	{
+		result = result * 10 + *nptr - '0';
+		nptr++;
+	}
+	return (sign * result);
+}
